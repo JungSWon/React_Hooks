1d088a4ec54cae6c4096cc499c58a53106d1ed2a
unknown <sw.onandon@gmail.com>
2020-07-13 19:53:23 +0900

    0713 : fin chapter #2.7 useNotification

--- react_hooks/src/index.js
+++ react_hooks/src/index.js
@@ -1,59 +1,31 @@
-import React, { useRef } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-// 이벤트를 받아 전체화면으로 만들기
-const useFullscreen = callback => {
-  const element = useRef();
-
-  const runCb = isFull => {
-    if (callback && typeof callback === "function") {
-      callback(isFull);
+// OS에 뜨는 브라우저 알람
+// notification API : https://developer.mozilla.org/en-US/docs/Web/API/notification
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNoif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const triggerFull = () => {
-    if (element.current) {
-      if (element.current.requestFullscreen) {
-        element.current.requestFullscreen();
-      } else if (element.current.mozRequestFullScreen) {
-        element.current.mozRequestFullScreen();
-      } else if (element.current.webkitRequestFullscreen) {
-        element.current.webkitRequestFullscreen();
-      } else if (element.current.msRequestFullscreen) {
-        element.current.msRequestFullscreen();
-      }
-      runCb(true);
-    }
-  };
-  const exitFull = () => {
-    console.log(document.exitFullscreen);
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-    runCb(false);
-  };
-  return { element, triggerFull, exitFull };
+  return fireNoif;
 };
 
 const App = () => {
-  const { element, triggerFull, exitFull } = useFullscreen();
-  return (
-      <div className="App" style={{ height: "1000vh" }}>
-        <img
-            ref={element}
-            src="https://images.unsplash.com/photo-1593642703055-4b72c180d9b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-            width="300"
-            alt="fullScreenImg"
-            onClick={exitFull}
-        />
-        <button onClick={triggerFull}>FULL SCREEN</button>
-      </div>
-  );
+  const triggerNotif = useNotification("BBUUUUUUUUU!!", { body: "Ewwwwww.." });
+  return <button onClick={triggerNotif}>NOTICE</button>;
 };
 
 const rootElement = document.getElementById("root");
