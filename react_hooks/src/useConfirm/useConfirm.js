1ddc2da300e9159bc53ba9208e95463ff0dd2c8b
unknown <sw.onandon@gmail.com>
2020-07-13 11:51:21 +0900

    0713 : on chapter #2.3 - edit useConfirm

--- react_hooks/src/useConfirm/useConfirm.js
+++ react_hooks/src/useConfirm/useConfirm.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 // 이벤트 실행을 일단 막고, 컨펌 후 실행
-export const useConfirm = (message='', onCallback, onCancel) => {
-    if (typeof onCallback && onCallback !== "function") {
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+    if (!onConfirm || typeof onConfirm !== "function") {
+        return;
+    }
+    if (onCancel && typeof onCancel !== "function") {
         return;
     }
     const confirmAction = () => {
         // confirm in JS : 확인창 띄우기
-        if (confirm(message)) {
-            onCallback();
+        if (window.confirm(message)) {
+            onConfirm();
         } else {
             // confirm에서 취소를 눌렀을때
             onCancel();
